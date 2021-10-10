5b4ccc2c16a486757f19c526ca8744ac1a3bfde9
second2050 <git@second2050.me>
2021-10-10 22:06:42 +0200

    fixed phase3_base_config.sh

--- phase3_base_config.sh
+++ phase3_base_config.sh
@@ -11,19 +11,21 @@ echo "INFO: Setting locale"
 sed -i '133s/.//' /etc/locale.gen # de_DE.UTF-8 UTF-8 # this uncomments the 133rd line
 sed -i '160s/.//' /etc/locale.gen # en_GB.UTF-8 UTF-8
 sed -i '177s/.//' /etc/locale.gen # en_US.UTF-8 UTF-8
-{ echo "LANG=en_US.UTF-8"
-  echo "LC_CTYPE=en_US.UTF-8"
-  echo "LC_NUMERIC=de_DE.UTF-8"
-  echo "LC_TIME=en_GB.UTF-8"
-  echo "LC_COLLATE=en_US.UTF-8"
-  echo "LC_MONETARY=de_DE.UTF-8"
-  echo "LC_MESSAGES=en_US.UTF-8"
-  echo "LC_PAPER=de_DE.UTF-8"
-  echo "LC_NAME=de_DE.UTF-8"
-  echo "LC_ADDRESS=de_DE.UTF-8"
-  echo "LC_TELEPHONE=de_DE.UTF-8"
-  echo "LC_MEASUREMENT=de_DE.UTF-8"
-  echo "LC_IDENTIFICATION=de_DE.UTF-8" } > /etc/locale.conf
+{ 
+    echo "LANG=en_US.UTF-8"
+    echo "LC_CTYPE=en_US.UTF-8"
+    echo "LC_NUMERIC=de_DE.UTF-8"
+    echo "LC_TIME=en_GB.UTF-8"
+    echo "LC_COLLATE=en_US.UTF-8"
+    echo "LC_MONETARY=de_DE.UTF-8"
+    echo "LC_MESSAGES=en_US.UTF-8"
+    echo "LC_PAPER=de_DE.UTF-8"
+    echo "LC_NAME=de_DE.UTF-8"
+    echo "LC_ADDRESS=de_DE.UTF-8"
+    echo "LC_TELEPHONE=de_DE.UTF-8"
+    echo "LC_MEASUREMENT=de_DE.UTF-8"
+    echo "LC_IDENTIFICATION=de_DE.UTF-8"
+} > /etc/locale.conf
 
 # set keyboard layout for vconsole
 echo "INFO: Setting keyboard layout for vconsole..."
@@ -34,9 +36,11 @@ echo "INFO: Setting hostname..."
 read -p "hostname? [second2050]: "
 _hostname=${parameter:-second2050}
 echo $_hostname > /etc/hostname
-{ echo "127.0.0.1    localhost"
-  echo "::1          localhost"
-  echo "127.0.1.1    $_hostname" } >> /etc/hosts
+{ 
+    echo "127.0.0.1    localhost"
+    echo "::1          localhost"
+    echo "127.0.1.1    $_hostname"
+} >> /etc/hosts
 
 # recreate initramfs
 echo "INFO: Recreating initramfs..."
