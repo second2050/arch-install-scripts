d79a0aedf82ed816467bba853709614342899f60
second2050 <git@second2050.me>
2021-10-11 20:25:14 +0200

    added full setup scripts

---
+++ setup_full_nobootmgr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# second2050 arch install script - FULL execution (Phase 2 and later) without desktop environment
+if [[ $1 == "phase3" ]]; then
+    ./phase3_base_config.sh
+    exit
+else
+    echo "THIS WILL START THE INSTALLATION OF ARCH"
+    echo "ARE YOU SURE YOU WANT TO DO THIS?"
+    echo "THIS WILL RUN ALL SCRIPTS BACK-TO-BACK!"
+    echo ""
+    echo "Chosen Setup: No Bootmanager"
+    bash phase2_base_install.sh
+    cp . /mnt/root/arch_install_scripts
+    arch-chroot /mnt /mnt/root/arch_install_scripts/setup_second2050.sh phase3
+fi;
+
